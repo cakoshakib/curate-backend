83131d245f25108702750c36b98bc0776a7705e3
Shakib Rahman <cakoshakib@gmail.com>
2024-10-13 13:14:58 -0400

    gin Hello, World!

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+func helloHandler(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello, curate!"})
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := gin.Default()
+	router.GET("/hello", helloHandler)
+	router.Run(":8080")
 }
